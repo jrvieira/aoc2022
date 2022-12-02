7e06f4c29b0ca98e1849c62bc68445c280dbc5cb
jrvieira <github@jrvieira.com>
2022-12-02 02:10:12 +0000

    day 01

--- src/Α01.hs
+++ src/Α01.hs
@@ -1,30 +1,32 @@
 module Α01 where
 
--- import Zero.Zero
+import Zero.Zero
+import Data.List ( sort )
+import Data.List.Split ( splitOn )
 
 test :: IO ()
 test = do
--- input <- parse <$> readFile "./tests/01.txt"
--- teqt "part 1" undefined $ part1 input
--- teqt "part 2" undefined $ part2 input
+   input <- parse <$> readFile "./tests/01.txt"
+   teqt "part 1" 24000 $ part1 input
+   teqt "part 2" 45000 $ part2 input
    pure ()
 
 main :: IO ()
 main = do
--- input <- parse <$> readFile "./input/01.txt"
--- print $ part1 input
--- print $ part2 input
+   input <- parse <$> readFile "./input/01.txt"
+   print $ part1 input
+   print $ part2 input
    pure ()
 
-parse :: String -> String
-parse = id
+parse :: String -> [[Int]]
+parse = map (map read . lines) . splitOn "\n\n"
 
 -- part 1
 
-part1 :: a
-part1 = undefined
+part1 :: [[Int]] -> Int
+part1 = maximum . map sum
 
 -- part 2
 
-part2 :: a
-part2 = undefined
+part2 :: [[Int]] -> Int
+part2 = sum . take 3 . reverse . sort . map sum
