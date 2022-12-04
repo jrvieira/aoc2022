8159e091ad877b5995bcdf89da7d7234f5f67b61
jrvieira <github@jrvieira.com>
2022-12-04 01:00:16 +0000

    day 03

--- src/Γ03.hs
+++ src/Γ03.hs
@@ -1,30 +1,39 @@
 module Γ03 where
 
--- import Zero.Zero
+import Zero.Zero
+
+import Data.List.Split ( chunksOf )
 
 test :: IO ()
 test = do
--- input <- parse <$> readFile "./tests/03.txt"
--- teqt "part 1" undefined $ part1 input
--- teqt "part 2" undefined $ part2 input
+   input <- parse <$> readFile "./tests/03.txt"
+   teqt "part 1" 157 $ part1 input
+   teqt "part 2" 70 $ part2 input
    pure ()
 
 main :: IO ()
 main = do
--- input <- parse <$> readFile "./input/03.txt"
--- print $ part1 input
--- print $ part2 input
+   input <- parse <$> readFile "./input/03.txt"
+   print $ part1 input
+   print $ part2 input
    pure ()
 
-parse :: String -> String
-parse = id
+parse :: String -> [Rucksack]
+parse = lines
+
+type Rucksack = [Char]
 
 -- part 1
 
-part1 :: a
-part1 = undefined
+part1 :: [Rucksack] -> Int
+part1 = sum . map (priority . head . uncurry (∩) . rucksack)
+
+rucksack l = splitAt (div (length l) 2) l
+
+priority :: Char -> Int
+priority c = succ $ length $ takeWhile (/= c) $ ['a'..'z'] <> ['A'..'Z']
 
 -- part 2
 
-part2 :: a
-part2 = undefined
+part2 :: [Rucksack] -> Int
+part2 = sum . map (priority . head . foldr1 (∩)) . chunksOf 3
