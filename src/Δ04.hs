8499acc6615ba0a5e98b259d881a6be5b4b61e36
jrvieira <github@jrvieira.com>
2022-12-04 11:27:33 +0000

    day 04

--- src/Δ04.hs
+++ src/Δ04.hs
@@ -1,30 +1,38 @@
 module Δ04 where
 
--- import Zero.Zero
+import Zero.Zero
 
 test :: IO ()
 test = do
--- input <- parse <$> readFile "./tests/04.txt"
--- teqt "part 1" undefined $ part1 input
--- teqt "part 2" undefined $ part2 input
+   input <- parse <$> readFile "./tests/04.txt"
+   teqt "part 1" 2 $ part1 input
+   teqt "part 2" 4 $ part2 input
    pure ()
 
 main :: IO ()
 main = do
--- input <- parse <$> readFile "./input/04.txt"
--- print $ part1 input
--- print $ part2 input
+   input <- parse <$> readFile "./input/04.txt"
+   print $ part1 input
+   print $ part2 input
    pure ()
 
-parse :: String -> String
-parse = id
+parse :: String -> [[Int]]
+parse = map parseNums . lines
 
 -- part 1
 
-part1 :: a
-part1 = undefined
+part1 :: [[Int]] -> Int
+part1 = length . filter contained
+
+contained :: [Int] -> Bool
+contained [a,b,c,d] = and [a <= c,b >= d] || and [a >= c,b <= d]
+contained _ = error "illegal assignment"
 
 -- part 2
 
-part2 :: a
-part2 = undefined
+part2 :: [[Int]] -> Int
+part2 = length . filter overlap
+
+overlap :: [Int] -> Bool
+overlap [a,b,c,d] = not . null $ [a..b] ∩ [c..d]
+overlap _ = error "illegal assignment"
