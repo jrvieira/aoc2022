61752d826130bbce3a1e9f6933787a7bfe2cf012
jrvieira <github@jrvieira.com>
2022-12-19 05:18:46 +0000

    day 11

--- src/Λ11.hs
+++ src/Λ11.hs
@@ -2,27 +2,31 @@ module Λ11 where
 
 import Zero.Zero
 
+import Data.List ( foldl', sortBy )
 import Data.List.Split ( splitOn )
+import Data.IntMap ( IntMap )
+import Data.IntMap qualified as I
 
 test :: IO ()
 test = do
    input <- parse <$> readFile "./tests/11.txt"
-   teqt "part 1" "" $ part1 input
--- teqt "part 2" undefined $ part2 input
+   teqt "part 1" 10605 $ part1 input
+   teqt "part 2" 2713310158 $ part2 input
    pure ()
 
 main :: IO ()
 main = do
    input <- parse <$> readFile "./input/11.txt"
    print $ part1 input
--- print $ part2 input
+   print $ part2 input
    pure ()
 
-parse :: String -> [Monkey]
-parse = map monkey . splitOn "\n\n"
+parse :: String -> IntMap Monkey
+parse = I.fromList . map monkey . splitOn "\n\n"
 
 data Monkey = Monkey {
-   id :: Int , items :: [Int] ,
+   inspections :: Int ,
+   items :: [Int] ,
    operation :: (Item,Int -> Int -> Int,Item) ,
    mt :: Test }
 
@@ -31,12 +35,12 @@ data Test = Test { d :: Int , t :: Int , f :: Int }
    deriving ( Show )
 
 instance Show Monkey where
-   show (Monkey i its op mt@(Test td tt tf)) = unwords ["M",show i,show its,show mt]
+   show (Monkey _ its _ tst) = unwords ["M",show its,show tst]
 
-monkey :: String -> Monkey
+monkey :: String -> (Int,Monkey)
 monkey = go . lines
    where
-   go [i,its,op,td,tt,tf] = Monkey (head $ parseNums i) (parseNums its) (itemL op,pop op,itemR op) (Test (head $ parseNums td) (head $ parseNums tt) (head $ parseNums tf))
+   go [i,its,op,td,tt,tf] = (head (parseNums i) ,) $ Monkey 0 (parseNums its) (itemL op,pop op,itemR op) (Test (head $ parseNums td) (head $ parseNums tt) (head $ parseNums tf))
    go _ = error "?"
 
 itemL :: String -> Item
@@ -55,16 +59,62 @@ pop :: String -> Int -> Int -> Int
 pop s
    | '*' ∈ s = (*)
    | '+' ∈ s = (+)
+   | otherwise = error "?"
 
 -- part 1
 
-part1 :: [Monkey] -> String
-part1 = show
+part1 :: IntMap Monkey -> Int
+part1 = mbusiness . (!! 20) . iterate mround
 
-worry = div 3
+mbusiness :: IntMap Monkey -> Int
+mbusiness = product . take 2 . sortBy (flip compare) . map inspections . I.elems
+
+mround :: IntMap Monkey -> IntMap Monkey
+mround = go 0
+   where
+   go i ms
+      | Just m  <- ms I.!? i = go (succ i) $ throws (i,m) ms
+      | Nothing <- ms I.!? i = ms
+
+throws :: (Int,Monkey) -> IntMap Monkey -> IntMap Monkey
+throws (i,m) ms = foldl' throw (I.insert i (m { inspections = inspections m + length (items m) , items = [] }) ms) (items m)
+   where
+   throw :: IntMap Monkey -> Int -> IntMap Monkey
+   throw ms' w = I.adjust (\m' -> m' { items = w' : items m' }) i' ms'
+      where
+      i'
+         | rem w' (d $ mt m) == 0 = t $ mt m
+         | otherwise = f $ mt m
+      w' = div (num l `op` num r) 3
+      (l,op,r) = operation m
+      num n
+         | Item x <- n = x
+         | Old    <- n = w
 
 -- part 2
 
-part2 :: a
-part2 = undefined
+part2 :: IntMap Monkey -> Int
+part2 = mbusiness . (!! 10000) . iterate mround'
 
+mround' :: IntMap Monkey -> IntMap Monkey
+mround' = go 0
+   where
+   go i ms
+      | Just m  <- ms I.!? i = go (succ i) $ throws' (i,m) ms
+      | Nothing <- ms I.!? i = ms
+
+throws' :: (Int,Monkey) -> IntMap Monkey -> IntMap Monkey
+throws' (i,m) ms = foldl' throw (I.insert i (m { inspections = inspections m + length (items m) , items = [] }) ms) (items m)
+   where
+   deworry = product $ map (d . mt) $ I.elems ms
+   throw :: IntMap Monkey -> Int -> IntMap Monkey
+   throw ms' w = I.adjust (\m' -> m' { items = w' : items m' }) i' ms'
+      where
+      i'
+         | rem w' (d $ mt m) == 0 = t $ mt m
+         | otherwise = f $ mt m
+      w' = mod (num l `op` num r) deworry
+      (l,op,r) = operation m
+      num n
+         | Item x <- n = x
+         | Old    <- n = w
