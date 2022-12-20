c78cee8f5f8274951b15d0e72c9049307cc6e78b
jrvieira <github@jrvieira.com>
2022-12-20 06:00:33 +0000

    day 14

--- src/Ξ14.hs
+++ src/Ξ14.hs
@@ -1,30 +1,63 @@
 module Ξ14 where
 
--- import Zero.Zero
+import Zero.Zero
+
+import Data.List.Split ( splitOn )
+import Data.Set ( Set )
+import Data.Set qualified as S
 
 test :: IO ()
 test = do
--- input <- parse <$> readFile "./tests/14.txt"
--- teqt "part 1" undefined $ part1 input
--- teqt "part 2" undefined $ part2 input
+   input <- parse <$> readFile "./tests/14.txt"
+   teqt "part 1" 24 $ part1 input
+   teqt "part 2" 93 $ part2 input
    pure ()
 
 main :: IO ()
 main = do
--- input <- parse <$> readFile "./input/14.txt"
--- print $ part1 input
--- print $ part2 input
+   input <- parse <$> readFile "./input/14.txt"
+   print $ part1 input
+   print $ part2 input
    pure ()
 
-parse :: String -> String
-parse = id
+parse :: String -> Set (Int,Int)
+parse = S.fromList  .  concatMap (draw . map (\t -> read $ "(" <> t <> ")") . splitOn " -> ") . lines
+
+draw :: [(Int,Int)] -> [(Int,Int)]
+draw ((xa,ya):(xb,yb):r) = [ (x,y) | x <- [min xa xb..max xa xb] , y <- [min ya yb..max ya yb] ] <> draw ((xb,yb):r)
+draw r = r
 
 -- part 1
 
-part1 :: a
-part1 = undefined
+part1 :: Set (Int,Int) -> Int
+part1 s = S.size (pour s) - S.size s
+   where
+   abyss = S.findMax $ S.map snd s
+   pour :: Set (Int,Int) -> Set (Int,Int)
+   pour s' = go (500,0)
+      where
+      go (x,y)
+      -- | False  # show (abyss,x,y) = undefined
+         | y > abyss            = s'
+         | (     x,succ y) ∉ s' = go (     x,succ y)
+         | (pred x,succ y) ∉ s' = go (pred x,succ y)
+         | (succ x,succ y) ∉ s' = go (succ x,succ y)
+         | otherwise            = pour $ S.insert (x,y) s'  -- # show (x,y)
 
 -- part 2
 
-part2 :: a
-part2 = undefined
+part2 :: Set (Int,Int) -> Int
+part2 s = S.size (pour (500,0) s) - S.size s
+   where
+   abyss = S.findMax $ S.map snd s
+   pour :: (Int,Int) -> Set (Int,Int) -> Set (Int,Int)
+   pour o = go o o
+      where
+      go mem (x,y) s'
+      -- | False  # show (abyss,x,y) = undefined
+         | y > abyss              = go o mem $ S.insert (x,y) s'
+         | (     x,succ y) ∉ s'   = go (x,y) (     x,succ y) s'
+         | (pred x,succ y) ∉ s'   = go (x,y) (pred x,succ y) s'
+         | (succ x,succ y) ∉ s'   = go (x,y) (succ x,succ y) s'
+         | (500,0) <- (x,y)       = S.insert (x,y) s'
+         | otherwise              = go o mem $ S.insert (x,y) s'
