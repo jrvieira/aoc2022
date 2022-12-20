27016b9fb41c3f7b6a57251bf52e9d940c1ca6ed
jrvieira <github@jrvieira.com>
2022-12-20 18:13:16 +0000

    clean up

--- src/Ξ14.hs
+++ src/Ξ14.hs
@@ -46,31 +46,12 @@ part1 s = S.size (pour s) - S.size s
 
 -- part 2
 
--- part2 :: Set (Int,Int) -> Int
--- part2 s = S.size (pour (500,0) s) - S.size s
---    where
---    abyss = S.findMax $ S.map snd s
---    pour :: (Int,Int) -> Set (Int,Int) -> Set (Int,Int)
---    pour o = go o o
---       where
---       go mem (x,y) s'
---       -- | False  # show (abyss,x,y) = undefined
---          | y > abyss              = go o mem $ S.insert (x,y) s'
---          | (     x,succ y) ∉ s'   = go (x,y) (     x,succ y) s'
---          | (pred x,succ y) ∉ s'   = go (x,y) (pred x,succ y) s'
---          | (succ x,succ y) ∉ s'   = go (x,y) (succ x,succ y) s'
---          | (500,0) <- (x,y)       = S.insert (x,y) s'
---          | otherwise              = go o mem $ S.insert (x,y) s'
-
--- automata
-
 part2 :: Set (Int,Int) -> Int
 part2 s = length $ concat $ scanl sim [(500,0)] $ depth <$> [1..succ abyss]
    where
    abyss = S.findMax $ S.map snd s
-   depth d = [ (x,d) | x <- (+ 500) <$> [negate d..d] , (x,d) ∉ s ]
-   sim a b = filter f b
+   depth d = filter (∉ s) $ (,d) . (+ 500) <$> [negate d..d]
+   sim a = filter f
       where
-      f (500,0) = True
       f (x',y') = or [ (x,pred y') ∈ a | x <- [pred x'..succ x'] ]
 
